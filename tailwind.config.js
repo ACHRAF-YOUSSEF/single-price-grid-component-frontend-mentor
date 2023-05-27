5a46a23896d568723b53cd66e6f823a55715c564
Achraf-Youssef <100535820+Achraf-Youssef@users.noreply.github.com>
2023-05-27 19:59:51 +0100

    hot fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     screens: {
-      sm: "375px",
+      sm: "600px",
       xl: "1440px",
     },
     extend: {
